6e197e8d87cda170ae449834b3916d239de5619c
Eugen Ciur <eugen@django-lessons.com>
2020-06-14 09:30:50 +0200

    now depends only on hocron

--- lidl_receipts_de/extract.py
+++ lidl_receipts_de/extract.py
@@ -1,3 +1,6 @@
+import re
+
+from hocron import Hocron, LinePattern
 
 
 def extract(hocr_file_path):
@@ -9,4 +12,17 @@ def extract(hocr_file_path):
         },
         'comp_key': []
     }
+    hocr = Hocron(hocr_file_path)
+    price_line_pattern = LinePattern(
+        ['EUR', re.compile('\d+[\.,]\d\d$')]  # noqa
+    )
+    date_line_pattern = LinePattern(
+        ['Datum', re.compile('\d\d.\d\d.\d\d')]  # noqa
+    )
+    price = hocr.get_labeled_value(price_line_pattern)
+    _date = hocr.get_labeled_value(date_line_pattern)
+
+    result['simple_keys']['price'] = price
+    result['simple_keys']['date'] = _date
+
     return result
--- lidl_receipts_de/identify.py
+++ lidl_receipts_de/identify.py
@@ -1,4 +1,15 @@
+import re
+
+from hocron import Hocron
 
 
 def identify(hocr_file_path):
-    return False
+    hocr = Hocron(hocr_file_path)
+    first_word = hocr.first_word
+
+    if not first_word:
+        return False
+
+    pattern = re.compile('L.D.$')
+
+    return re.match(pattern, first_word)
